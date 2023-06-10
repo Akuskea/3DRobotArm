c09f1beae310cf24e215e2034815dc4341ecd1ad
Akuskea <102030185+Akuskea@users.noreply.github.com>
2023-06-09 19:45:21 -0500

    Update 3D_robotArm.py
    Implementation of the getTransformation function, draw a more realistic robot arm

--- 3D_robotArm.py
+++ 3D_robotArm.py
@@ -12,161 +12,100 @@ import time
 from vedo import *
 from os import path
 
+def getTransformation(t, phi, rot):
 
-# In[73]:
-def kinematicStart(phi1, phi2, phi3, phi4):
-    L1 = 3
-    #Change the L2 to see the full arm movement
-    L2 = 60
+    if phi == -1:  # no rotation
+        R = np.eye(3)
 
+    else:
+        
+        c = np.cos(phi * np.pi / 180)
+        s = np.sin(phi * np.pi / 180)
+        
+        if rot == 'x':
+          R = np.array(
+              [[1, 0, 0],
+              [0, c, -s],
+              [0, s, c]]
+          )
+        elif rot == 'y':
+          R = np.array(
+              [[c, 0, s],
+              [0, 1, 0],
+              [-s, 0, c]]
+          )
+        else: # rot = 'z'
+          R = np.array(
+              [[c, -s, 0],
+              [s, c, 0],
+              [0, 0, 1]]
+          )
+
+    T = np.block([[R, t],
+                  [np.zeros((1, 3)), 1]])
+
+    return T
+
+def kinematic(phi1,phi2, phi3, phi4):
+    phi = np.array([phi1, phi2, phi3, phi4])
+    L1 = 6
+    L2 = 9
     L3 = 5
     L4 = 4
-    T_01 = np.array([[np.cos(phi1), -np.sin(phi1), 0.0, 0.0],
-                     [np.sin(phi1), np.cos(phi1), 0.0, 0.0],
-                     [0.0, 0.0, 1.0, 0.0],
-                     [0.0, 0.0, 0.0, 1.0]])
-
-    T1_2 = np.array([[np.cos(phi2), -np.sin(phi2), 0.0, 0.0],
-                     [np.sin(phi2), np.cos(phi2), 0.0, 0.0],
-                     [0.0, 0.0, 1.0, L1],
-                     [0.0, 0.0, 0.0, 1.0]])
-
-    T2_3 = np.array([[np.cos(phi3), -np.sin(phi3), 0.0, 0.0],
-                     [np.sin(phi3), np.cos(phi3), 0.0, 0.0],
-                     [0.0, 0.0, 1.0, L2],
-                     [0.0, 0.0, 0.0, 1.0]])
-
-    T3_4 = np.array([[np.cos(phi4), -np.sin(phi4), 0.0, 0.0],
-                     [np.sin(phi4), np.cos(phi4), 0.0, 0.0],
-                     [0.0, 0.0, 1.0, L3],
-                     [0.0, 0.0, 0.0, 1.0]])
-
-    T4_5 = np.array([[1.0, 0.0 ,0.0, 0.0],
-                     [0.0, 1.0, 0.0, 0.0],
-                     [0.0, 0.0, 1.0, L4],
-                     [0.0, 0.0, 0.0, 1.0]])
-
-    #Local to global
-    T_02 = T_01 @ T1_2
-    T_03 = T_01 @ T1_2 @ T2_3
-    T_04 = T_01 @ T1_2 @ T2_3 @ T3_4
-    T_05 = T_01 @ T1_2 @ T2_3 @ T3_4 @ T4_5
-
-    # Define the coordinates of first joint and create sphere
-    p1 = np.array([0.0, 0.0, 0.0, 1.0])
-    p1 = p1[:3]
-    s1=Sphere(p1, r=0.5, c='black') 
-
-    # Calculate the global coordinates of joint 2 and create sphere
-    j2_local = np.array([0.0, 0.0, L1, 1])
-    p2 = T_01 @ j2_local
-    p2 = p2[:3]
-    s2=Sphere(p2, r=1, c='white') 
-
-    # Calculate the global coordinates of joint 3 and create sphere
-    j3_local = np.array([0.0, 0.0, L2, 1])
-    p3 = T_02 @ j3_local
-    p3 = p3[:3]
-    s3=Sphere(p3, r=1, c='white') 
-
-    # Calculate the global coordinates of joint 4 and create sphere
-    j4_local = np.array([0.0, 0.0, L3, 1])
-    p4 = T_04 @ j4_local
-    p4 = p4[:3]
-    s4=Sphere(p4, r=1, c='white') 
-
-    # Calculate the global coordinates of joint 5 and create sphere
-    j5_local = np.array([0.0, 0.0, L4, 1])
-    p5 = T_05 @ j5_local
-    p5 = p5[:3]
-    s5=Sphere(p5, r=0.5, c='black') 
+    p1 = np.array([[0],[0],[0]])
+    p2 = np.array([[0],[0],[L1]])
+    p3 = np.array([[0],[0],[L2]])
+    p4 = np.array([[0],[0],[L3]])
+    p5 = np.array([[0],[0],[L4]])
     
+    # Local transformation matrices 
     
-    #Create cylinder arms
-    c1 = Cylinder(pos=[p1,p2],r=3, axis=(0, 0, 1), cap=True, res=24, c='grey',alpha=1)
-
-    c2 = Cylinder(pos=[p2,p3],r=1, axis=(0, 0, 1), cap=True, res=24, c='grey',alpha=1)
-
-    c3 = Cylinder(pos=[p3,p4],r=1, axis=(0, 0, 1), cap=True, res=24, c='grey',alpha=1)
-
-    c4 = Cylinder(pos=[p4,p5],r=1, axis=(0, 0, 1), cap=True, res=24, c='grey',alpha=1)
-
-    return s1, s2, s3, s4,s5, c1, c2, c3, c4
-    
-
-def kinematic(phi1, phi2, phi3, phi4):
-    L1 = 3
-    L2 = 8
-    L3 = 5
-    L4 = 4
-    T_01 = np.array([[np.cos(phi1), -np.sin(phi1), 0.0, 0.0],
-                     [np.sin(phi1), np.cos(phi1), 0.0, 0.0],
-                     [0.0, 0.0, 1.0, 0.0],
-                     [0.0, 0.0, 0.0, 1.0]])
-
-    T1_2 = np.array([[np.cos(phi2), 0.0 ,np.sin(phi2), 0.0],
-                     [-np.sin(phi2), 1.0, np.cos(phi2), 0.0],
-                     [0.0, 0.0, 1.0, L1],
-                     [0.0, 0.0, 0.0, 1.0]])
-
-    T2_3 = np.array([[np.cos(phi3), 0.0 ,np.sin(phi3), 0.0],
-                     [-np.sin(phi3), 1.0, np.cos(phi3), 0.0],
-                     [0.0, 0.0, 1.0, L2],
-                     [0.0, 0.0, 0.0, 1.0]])
-
-    T3_4 = np.array([[np.cos(phi4), 0.0 ,np.sin(phi4), 0.0],
-                     [-np.sin(phi4), 1.0, np.cos(phi4), 0.0],
-                     [0.0, 0.0, 1.0, L3],
-                     [0.0, 0.0, 0.0, 1.0]])
-
-    T4_5 = np.array([[1.0, 0.0 ,0.0, 0.0],
-                     [0.0, 1.0, 0.0, 0.0],
-                     [0.0, 0.0, 1.0, L4],
-                     [0.0, 0.0, 0.0, 1.0]])
+     
+    T_01 = getTransformation(p1, phi[0],'z')
+    T1_2 = getTransformation(p2, phi[1],'y')
+    T2_3 = getTransformation(p3, phi[2],'y')
+    T3_4 = getTransformation(p4, phi[3],'y')
+    T4_5 = np.array([
+        [1, 0, 0, 0],
+        [0, 1, 0, 0],
+        [0, 0, 1, L4],
+        [0, 0, 0, 1]
+    ])
 
     #Local to global
     T_02 = T_01 @ T1_2
     T_03 = T_02 @ T2_3
     T_04 = T_03 @ T3_4
     T_05 = T_04 @ T4_5
-    
 
     # Define the coordinates of first joint and create sphere
-    p1 = np.array([0.0, 0.0, 0.0, 1.0])
-    p1 = p1[:3]
-    s1=Sphere(p1, r=0.5, c='black') 
+    p1 = [0,0,0]
+    s1=Sphere(p1, r=0.5, c='black')
 
-    # Calculate the global coordinates of joint 2 and create sphere
-    j2_local = np.array([0.0, 0.0, L1, 1])
-    p2 = T_01 @ j2_local
-    p2 = p2[:3]
-    s2=Sphere(p2, r=1, c='white') 
+    # Joint 2
+    p2 = T_02[:3,3]
+    s2=Sphere(p2, r=5, c='purple')
 
-    # Calculate the global coordinates of joint 3 and create sphere
-    j3_local = np.array([0.0, 0.0, L2, 1])
-    p3 = T_02 @ j3_local
-    p3 = p3[:3]
-    s3=Sphere(p3, r=1, c='white') 
+    # Joint 3
+    p3 = T_03[:3,3]
+    s3=Sphere(p3, r=3, c='purple') 
 
-    # Calculate the global coordinates of joint 4 and create sphere
-    j4_local = np.array([0.0, 0.0, L3, 1])
-    p4 = T_03 @ j4_local
-    p4 = p4[:3]
-    s4=Sphere(p4, r=1, c='white') 
+    # Joint 4
+    p4 = T_04[:3,3]
+    s4=Sphere(p4, r=2, c='black') 
 
-    # Calculate the global coordinates of joint 5 and create sphere
-    j5_local = np.array([0.0, 0.0, L4, 1])
-    p5 = T_04 @ j5_local
-    p5 = p5[:3]
-    s5=Sphere(p5, r=0.5, c='black', alpha=1) 
+    # Joint 5
+    p5 = T_05[:3,3]
+    s5=Sphere(p5, r=2, c='purple') 
+
     
 
     #Create cylinder arms
-    c1 = Cylinder(pos=[p1,p2],r=3, axis=(0, 0, 1), cap=True, res=24, c='light grey',alpha=1)
+    c1 = Cylinder(pos=[p1,p2],r=5, axis=(0, 0, 1), cap=True, res=24, c='purple',alpha=1)
 
-    c2 = Cylinder(pos=[p2,p3],r=1, axis=(0, 0, 1), cap=True, res=24, c='light grey',alpha=1)
+    c2 = Cylinder(pos=[p2,p3],r=3, axis=(0, 0, 1), cap=True, res=24, c='purple',alpha=1)
 
-    c3 = Cylinder(pos=[p3,p4],r=1,axis=(0, 0, 1), cap=True, res=24, c='light grey',alpha=1)
+    c3 = Cylinder(pos=[p3,p4],r=2,axis=(0, 0, 1), cap=True, res=24, c='black',alpha=1)
 
     c4 = Cylinder(pos=[p4,p5],r=1, axis=(0, 0, 1), cap=True, res=24, c='light grey',alpha=1)
     arm = s1 + s2 + s3 + s4+ s5 + c1 + c2 + c3 + c4
@@ -199,14 +138,14 @@ def loop_func(event):
 
 phi = np.pi/20
 
-#Video file
+# Video file
 video = Video("tmp.mp4", 
               backend='ffmpeg', 
               fps = 24
              ) 
 
 #Start position
-s1,s2,s3, s4,s5, c1, c2, c3, c4 = kinematicStart(0.0, 0.0, 0.0, 0.0)
+s1,s2,s3, s4,s5, c1, c2, c3, c4 = kinematic(0.0, 0.0, 0.0, 0.0)
 arm = s1 + s2 + s3 + s4+ s5 + c1 + c2 + c3 + c4
 
 plt = Plotter(size=(1050, 600))
